150ab729bda00e5c4fb0fc454bdea7a31f89840c
bielpeclat <bielpeclat1@gmail.com>
2026-07-01 09:59:21 +0100

    22_23_feito

--- ex_exames/22_23_normal_ex_2.asm
+++ ex_exames/22_23_normal_ex_2.asm
@@ -1,16 +1,66 @@
 MostrasLetras proc
 
-    ; string termina com 0
-    ; bx aponta para a string
-    ; al -> linha           ah -> coluna
-    ; dl -> direcao para escrever
+    push ax
+    push bx
+    push cx
+    push di
+    push dx
 
-    ; vamos calcular o offset
-    ; offset = (linha * 160) + (coluna * 2)
-    
+    ; calcular o offset
+    mov cl, ah
+    xor ch, ch
+    shl cx, 1 ; multiplicar por 2
 
-    
-    
+    xor ah, ah
+    mov dl, 160
+    mul dl ; ax = al * dl
+
+    mov di, ax
+    add di, cx
+    pop dx ; para ter a direcao do deslocamento
+
+    cmp dl, 1
+    je hor_esq
+
+    cmp dl, 2
+    je vert_cima
+
+    cmp dl, 3
+    je vert_baixo
+
+hor_direita:
+    mov cx, 2
+    jmp escreve_loop
+
+hor_esq:
+    mov cx, -2
+    jmp escreve_loop
+
+vert_cima:
+    mov cx, -160
+    jmp escreve_loop
+
+vert_baixo:
+    mov cx, 160
+    jmp escreve_loop
+
+escreve_loop:
+    ; cx contem o deslocamento de cada iteracao
+    mov al, [bx]
+    cmp al, 0
+    je fim_mostra
+
+    mov es:[di], al
+    inc bx ; proxima letra
+    add di, cx
+
+    jmp escreve_loop
+
+fim_mostra:
+    pop di
+    pop cx
+    pop bx
+    pop ax
 
     ret
 MostrasLetras endp
